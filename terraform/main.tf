f4d2ec08b5429a0e76bac3694487df46cec4bf65
eshwark <eshwar.k@sonata-software.com>
2024-08-08 15:37:28 +0530

    five

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,6 +3,22 @@ provider "aws" {
   # Credentials will be provided through environment variables or Jenkins credentials
 }
 
+# Data source for default VPC
+data "aws_vpc" "default" {
+  default = true
+}
+
+# Data source for default subnets
+data "aws_subnets" "default" {
+  vpc_id = data.aws_vpc.default.id
+}
+
+# Data source for default security group
+data "aws_security_group" "default" {
+  vpc_id = data.aws_vpc.default.id
+  # Specify the default security group ID
+}
+
 # S3 bucket for Kops state management
 resource "aws_s3_bucket" "kops_state" {
   bucket = "my-kops-state-bucket" # Ensure this is globally unique
@@ -16,28 +32,36 @@ resource "aws_s3_bucket_versioning" "kops_state" {
   }
 }
 
-# Load Balancer
-resource "aws_elb" "example" {
+# Application Load Balancer
+resource "aws_lb" "example" {
   name               = "example-lb"
-  availability_zones = ["ap-south-1a", "ap-south-1b"]
-  
-  listener {
-    instance_port     = 80
-    instance_protocol = "HTTP"
-    lb_port           = 80
-    protocol          = "HTTP"
-  }
-  
-  health_check {
-    target              = "TCP:80"
-    interval            = 30
-    timeout             = 5
-    healthy_threshold   = 2
-    unhealthy_threshold = 2
-  }
-  
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [data.aws_security_group.default.id]
+  subnets            = data.aws_subnets.default.ids
+
+  enable_deletion_protection = false
+  idle_timeout               = 60
+  drop_invalid_header_fields = true
+  enable_http2               = true
+
   tags = {
     Name = "example-lb"
+  }
+}
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.example.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type = "fixed-response"
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "Hello, World!"
+      status_code  = "200"
+    }
   }
 }
 
@@ -46,5 +70,5 @@ output "s3_bucket" {
 }
 
 output "elb_dns_name" {
-  value = aws_elb.example.dns_name
+  value = aws_lb.example.dns_name
 }
