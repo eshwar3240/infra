b99984b8820af0ae4892536a44a8e26379de050d
eshwar3240 <157467990+eshwar3240@users.noreply.github.com>
2024-08-13 12:11:43 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,65 +4,65 @@ provider "aws" {
 }
 
 # Data source for default VPC
-data "aws_vpc" "eshwar_default" {
+data "aws_vpc" "eshwar1000_default" {
   default = true
 }
 
 # Data source for public subnets
-data "aws_subnets" "eshwar_public" {
+data "aws_subnets" "eshwar1000_public" {
   filter {
     name   = "vpc-id"
-    values = [data.aws_vpc.eshwar_default.id]
+    values = [data.aws_vpc.eshwar1000_default.id]
   }
 }
 
 # Data source for default security group
-data "aws_security_group" "eshwar_default" {
+data "aws_security_group" "eshwar1000_default" {
   filter {
     name   = "vpc-id"
-    values = [data.aws_vpc.eshwar_default.id]
+    values = [data.aws_vpc.eshwar1000_default.id]
   }
 }
 
 # S3 bucket for Kops state management
-resource "aws_s3_bucket" "eshwar_kops_state" {
-  bucket = "eshwar-kops-state-bucket" # Ensure this is globally unique
+resource "aws_s3_bucket" "eshwar1000_kops_state" {
+  bucket = "eshwar1000-kops-state-bucket" # Ensure this is globally unique
 }
 
 # Enable versioning for the S3 bucket
-resource "aws_s3_bucket_versioning" "eshwar_kops_state_versioning" {
-  bucket = aws_s3_bucket.eshwar_kops_state.id
+resource "aws_s3_bucket_versioning" "eshwar1000_kops_state_versioning" {
+  bucket = aws_s3_bucket.eshwar1000_kops_state.id
   versioning_configuration {
     status = "Disabled"
   }
 }
 
 # S3 bucket for Load Balancer logs
-resource "aws_s3_bucket" "eshwar_lb_logs" {
-  bucket = "eshwar-lb-logs-bucket" # Ensure this is globally unique
+resource "aws_s3_bucket" "eshwar1000_lb_logs" {
+  bucket = "eshwar1000-lb-logs-bucket" # Ensure this is globally unique
 }
 
 # Enable versioning for the S3 bucket used for load balancer logs
-resource "aws_s3_bucket_versioning" "eshwar_lb_logs_versioning" {
-  bucket = aws_s3_bucket.eshwar_lb_logs.id
+resource "aws_s3_bucket_versioning" "eshwar1000_lb_logs_versioning" {
+  bucket = aws_s3_bucket.eshwar1000_lb_logs.id
   versioning_configuration {
     status = "Enabled"
   }
 }
 
 # Application Load Balancer
-resource "aws_lb" "eshwar_test_lb" {
-  name               = "eshwar-test-lb-tf"
+resource "aws_lb" "eshwar1000_test_lb" {
+  name               = "eshwar1000-test-lb-tf"
   internal           = false
   load_balancer_type = "application"
-  security_groups    = [data.aws_security_group.eshwar_default.id]
-  subnets            = data.aws_subnets.eshwar_public.ids
+  security_groups    = [data.aws_security_group.eshwar1000_default.id]
+  subnets            = data.aws_subnets.eshwar1000_public.ids
 
   enable_deletion_protection = false
 
   access_logs {
-    bucket  = aws_s3_bucket.eshwar_lb_logs.id
-    prefix  = "eshwar-test-lb"
+    bucket  = aws_s3_bucket.eshwar1000_lb_logs.id
+    prefix  = "eshwar1000-test-lb"
     enabled = true
   }
 
@@ -71,8 +71,8 @@ resource "aws_lb" "eshwar_test_lb" {
   }
 }
 
-resource "aws_lb_listener" "eshwar_http_listener" {
-  load_balancer_arn = aws_lb.eshwar_test_lb.arn
+resource "aws_lb_listener" "eshwar1000_http_listener" {
+  load_balancer_arn = aws_lb.eshwar1000_test_lb.arn
   port              = 80
   protocol          = "HTTP"
 
@@ -87,13 +87,16 @@ resource "aws_lb_listener" "eshwar_http_listener" {
 }
 
 output "s3_bucket" {
-  value = aws_s3_bucket.eshwar_kops_state.bucket
+  value = aws_s3_bucket.eshwar1000_kops_state.bucket
 }
 
 output "lb_logs_bucket" {
-  value = aws_s3_bucket.eshwar_lb_logs.bucket
+  value = aws_s3_bucket.eshwar1000_lb_logs.bucket
 }
 
+output "elb_dns_name" {
+  value = aws_lb.eshwar1000_test_lb.dns_name
+}
 output "elb_dns_name" {
   value = aws_lb.eshwar_test_lb.dns_name
 }
